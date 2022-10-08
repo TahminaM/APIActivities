490b9b8ff57c91b8809151acd8063b32e1286554
TahminaM <73051805+TahminaM@users.noreply.github.com>
2022-10-07 20:54:06 -0400

    Add files via upload

---
+++ Activity_4/Activity_4/Program.cs
@@ -0,0 +1,88 @@
+﻿// Tahmina A Munni
+//Prof: Serra Canca 
+
+
+namespace MyConsoleApplication
+{
+
+    //Interface
+    interface MyInterface
+    {
+        // 3 Methods
+        int Age { get; set; }
+        string Name { get; set; }
+        bool IsAlive { get; set; }
+    }
+
+    class MyClass : MyInterface
+    {
+        private int age;
+        private string name;
+        private bool isAlive;
+
+        public int Age { get { return age; } set { age = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public bool IsAlive { get { return isAlive; } set { isAlive = value; } }
+
+
+    }
+
+    //Event
+    class DisplayInfo
+    {
+        // Define a delegate
+        public delegate void DisplayInfoEventHandler(object source, EventArgs args);
+
+        // Declare the event
+        public event DisplayInfoEventHandler InfoDisplayed;
+
+        public void ShowInfo(MyClass avenger)
+        {
+            Console.WriteLine ($"When Dad died, {avenger.Name} was {avenger.Age}.");
+            Thread.Sleep(3000);
+
+            OnInfoDisplayed();
+        }
+
+        // Raising the event
+        protected virtual void OnInfoDisplayed()
+        {
+            if (InfoDisplayed != null)
+                InfoDisplayed (this, null);
+        }
+
+    }
+
+    // Subscribing to the event
+    public class AppService
+    {
+        public void OnInfoDisplayed (object source, EventArgs eventargs)
+        {
+            Console.WriteLine("She Miss him!!");
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            var Avenger = new MyClass();
+
+            Avenger.Age = 4;
+            Avenger.Name = "Tahmina";
+            Avenger.IsAlive = false;
+
+            var displayInfo = new DisplayInfo();
+            var appService = new AppService();
+
+            displayInfo.InfoDisplayed += appService.OnInfoDisplayed;
+            displayInfo.ShowInfo(Avenger);
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
